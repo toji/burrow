135bfb775970efd488ff5a3375b3393703d922a5
Brandon Jones <tojiro@gmail.com>
2023-03-28 15:38:58 -0700

    Basic lighting!

--- src/deferred-renderer.ts
+++ src/deferred-renderer.ts
@@ -1,6 +1,8 @@
 import { TextureVisualizer } from './texture-visualizer.js'
-import { wgsl } from 'https://cdn.jsdelivr.net/npm/wgsl-preprocessor@1.0/wgsl-preprocessor.js'
+import { gBufferShader, lightingShader } from './shaders/deferred.js';
 import { mat4, vec3 } from '../node_modules/gl-matrix/esm/index.js';
+import { Mesh, createTempMesh } from './cube-mesh.js';
+import { LightSpriteRenderer } from './light-sprite.js';
 
 export enum DebugViewType {
   none = "none",
@@ -12,29 +14,16 @@ export enum DebugViewType {
   depth = "depth",
 };
 
-interface BufferBinding {
-  buffer: GPUBuffer,
-  offset: number,
-};
-
-interface VertexBufferBinding extends BufferBinding {
-  slot: number
-};
-
-interface IndexBufferBinding extends BufferBinding {
-  format: GPUIndexFormat
-};
-
-interface Mesh {
-  vertex: VertexBufferBinding[],
-  index?: IndexBufferBinding,
-  drawCount: number;
-}
-
 interface Camera {
   viewMatrix: mat4,
   position: vec3,
 }
+
+const MAX_LIGHTS = 64;
+const LIGHT_STRUCT_SIZE = 32;
+const LIGHT_BUFFER_SIZE = (MAX_LIGHTS * LIGHT_STRUCT_SIZE) + 16;
+
+const LIGHT_COUNT = 6;
 
 export class DeferredRenderer {
   attachmentSize: GPUExtent3DDictStrict = { width: 0, height: 0 }
@@ -46,17 +35,25 @@ export class DeferredRenderer {
   depthTexture: GPUTexture;
 
   colorAttachments: GPURenderPassColorAttachment[];
+  lightAttachments: GPURenderPassColorAttachment[];
   depthAttachment: GPURenderPassDepthStencilAttachment;
 
   textureVisualizer: TextureVisualizer;
 
   debugView: DebugViewType = DebugViewType.all;
 
-  cameraBindGroupLayout: GPUBindGroupLayout;
-  cameraBindGroup: GPUBindGroup;
+  frameBindGroupLayout: GPUBindGroupLayout;
+  frameBindGroup: GPUBindGroup;
   cameraBuffer: GPUBuffer;
+  lightBuffer: GPUBuffer;
+  lightArrayBuffer: ArrayBuffer;
+
+  gBufferBindGroupLayout: GPUBindGroupLayout;
+  gBufferBindGroup: GPUBindGroup;
 
   lightingPipeline: GPURenderPipeline;
+
+  lightSpriteRenderer: LightSpriteRenderer;
 
   tempPipeline: GPURenderPipeline;
   tempMesh: Mesh;
@@ -70,29 +67,92 @@ export class DeferredRenderer {
       usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     });
 
-    this.cameraBindGroupLayout = device.createBindGroupLayout({
-      label: 'camera bind group layout',
+    this.lightArrayBuffer = new ArrayBuffer(LIGHT_BUFFER_SIZE);
+    this.lightBuffer = device.createBuffer({
+      label: 'light storage buffer',
+      size: LIGHT_BUFFER_SIZE,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST
+    });
+
+    const lightCount = new Uint32Array(this.lightArrayBuffer, 0, 1);
+    lightCount[0] = LIGHT_COUNT;
+
+    const lightColors = [
+      [1.0, 0.3, 0.3,  2],
+      [0.3, 1.0, 0.3,  2],
+      [0.3, 0.3, 1.0,  2],
+      [1.0, 1.0, 0.3,  2],
+      [0.3, 1.0, 1.0,  2],
+      [1.0, 0.3, 1.0,  2],
+    ];
+
+    for (let i = 0; i < LIGHT_COUNT; ++i) {
+      const lightOffset = 16 + (i * LIGHT_STRUCT_SIZE)
+      const posRange = new Float32Array(this.lightArrayBuffer, lightOffset, 4);
+      const colorIntensity = new Float32Array(this.lightArrayBuffer, lightOffset + 16, 4);
+
+      const r = (i / LIGHT_COUNT) * Math.PI * 2;
+      posRange[0] = Math.sin(r) * 2.5;
+      posRange[1] = 0;
+      posRange[2] = Math.cos(r) * 2.5;
+      posRange[3] = 5;
+
+      colorIntensity.set(lightColors[i % LIGHT_COUNT]);
+    }
+
+    this.frameBindGroupLayout = device.createBindGroupLayout({
+      label: 'frame bind group layout',
       entries: [{
         binding: 0,
-        visibility: GPUShaderStage.VERTEX,
-        buffer: {}
+        visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+        buffer: {} // Camera uniforms
+      }, {
+        binding: 1,
+        visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+        buffer: { type: 'read-only-storage' } // Light uniforms
       }]
     });
 
-    this.cameraBindGroup = device.createBindGroup({
+    this.frameBindGroup = device.createBindGroup({
       label: 'camera bind group',
-      layout: this.cameraBindGroupLayout,
+      layout: this.frameBindGroupLayout,
       entries: [{
         binding: 0,
         resource: { buffer: this.cameraBuffer }
+      }, {
+        binding: 1,
+        resource: { buffer: this.lightBuffer }
       }]
     });
 
-    this.tempMesh = this.createTempMesh();
+    this.gBufferBindGroupLayout = device.createBindGroupLayout({
+      label: 'gBuffer bind group layout',
+      entries: [{
+        binding: 0,
+        visibility: GPUShaderStage.FRAGMENT,
+        texture: {}
+      }, {
+        binding: 1,
+        visibility: GPUShaderStage.FRAGMENT,
+        texture: {}
+      }, {
+        binding: 2,
+        visibility: GPUShaderStage.FRAGMENT,
+        texture: {}
+      }, {
+        binding: 3,
+        visibility: GPUShaderStage.FRAGMENT,
+        texture: { sampleType: 'depth' }
+      }]
+    });
+
+    this.tempMesh = createTempMesh(device);
 
     this.tempPipeline = this.createDeferredPipeline();
 
     this.lightingPipeline = this.createLightingPipeline();
+
+    this.lightSpriteRenderer = new LightSpriteRenderer(device, this.frameBindGroupLayout);
   }
 
   resize(width: number, height: number) {
@@ -158,7 +218,12 @@ export class DeferredRenderer {
       view: this.lightTexture.createView(),
       loadOp: 'clear',
       storeOp: 'store',
-      //clearValue: [1, 1, 0, 1],
+    }];
+
+    this.lightAttachments = [{
+      view: this.colorAttachments[3].view,
+      loadOp: 'load',
+      storeOp: 'store',
     }];
 
     this.depthAttachment = {
@@ -167,68 +232,24 @@ export class DeferredRenderer {
       depthStoreOp: 'store',
       depthClearValue: 1,
     };
-  }
 
-  createTempMesh(): Mesh {
-    const vertexArray = new Float32Array([
-      // float3 position, float3 color, float3 normal, float2 uv,
-      1, -1, 1,    1, 0, 1,  0, -1, 0,  1, 1,
-      -1, -1, 1,   1, 0, 1,  0, -1, 0,  0, 1,
-      -1, -1, -1,  1, 0, 1,  0, -1, 0,  0, 0,
-      1, -1, -1,   1, 0, 1,  0, -1, 0,  1, 0,
-      1, -1, 1,    1, 0, 1,  0, -1, 0,  1, 1,
-      -1, -1, -1,  1, 0, 1,  0, -1, 0,  0, 0,
-
-      1, 1, 1,     1, 0, 0,  1, 0, 0,  1, 1,
-      1, -1, 1,    1, 0, 0,  1, 0, 0,  0, 1,
-      1, -1, -1,   1, 0, 0,  1, 0, 0,  0, 0,
-      1, 1, -1,    1, 0, 0,  1, 0, 0,  1, 0,
-      1, 1, 1,     1, 0, 0,  1, 0, 0,  1, 1,
-      1, -1, -1,   1, 0, 0,  1, 0, 0,  0, 0,
-
-      -1, 1, 1,    0, 1, 0,  0, 1, 0,  1, 1,
-      1, 1, 1,     0, 1, 0,  0, 1, 0,  0, 1,
-      1, 1, -1,    0, 1, 0,  0, 1, 0,  0, 0,
-      -1, 1, -1,   0, 1, 0,  0, 1, 0,  1, 0,
-      -1, 1, 1,    0, 1, 0,  0, 1, 0,  1, 1,
-      1, 1, -1,    0, 1, 0,  0, 1, 0,  0, 0,
-
-      -1, -1, 1,   0, 1, 1,  -1, 0, 0,  1, 1,
-      -1, 1, 1,    0, 1, 1,  -1, 0, 0,  0, 1,
-      -1, 1, -1,   0, 1, 1,  -1, 0, 0,  0, 0,
-      -1, -1, -1,  0, 1, 1,  -1, 0, 0,  1, 0,
-      -1, -1, 1,   0, 1, 1,  -1, 0, 0,  1, 1,
-      -1, 1, -1,   0, 1, 1,  -1, 0, 0,  0, 0,
-
-      1, 1, 1,     0, 0, 1,  0, 0, 1,  1, 1,
-      -1, 1, 1,    0, 0, 1,  0, 0, 1,  0, 1,
-      -1, -1, 1,   0, 0, 1,  0, 0, 1,  0, 0,
-      -1, -1, 1,   0, 0, 1,  0, 0, 1,  0, 0,
-      1, -1, 1,    0, 0, 1,  0, 0, 1,  1, 0,
-      1, 1, 1,     0, 0, 1,  0, 0, 1,  1, 1,
-
-      1, -1, -1,   1, 1, 0,  0, 0, -1,  1, 1,
-      -1, -1, -1,  1, 1, 0,  0, 0, -1,  0, 1,
-      -1, 1, -1,   1, 1, 0,  0, 0, -1,  0, 0,
-      1, 1, -1,    1, 1, 0,  0, 0, -1,  1, 0,
-      1, -1, -1,   1, 1, 0,  0, 0, -1,  1, 1,
-      -1, 1, -1,   1, 1, 0,  0, 0, -1,  0, 0,
-    ]);
-
-    const vertexBuffer = this.device.createBuffer({
-      size: vertexArray.byteLength,
-      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+    this.gBufferBindGroup = this.device.createBindGroup({
+      label: 'gBuffer bind group',
+      layout: this.gBufferBindGroupLayout,
+      entries: [{
+        binding: 0,
+        resource: this.colorAttachments[0].view,
+      }, {
+        binding: 1,
+        resource: this.colorAttachments[1].view,
+      }, {
+        binding: 2,
+        resource: this.colorAttachments[2].view,
+      }, {
+        binding: 3,
+        resource: this.depthAttachment.view,
+      }]
     });
-    this.device.queue.writeBuffer(vertexBuffer, 0, vertexArray);
-
-    return {
-      vertex: [{
-        slot: 0,
-        buffer: vertexBuffer,
-        offset: 0
-      }],
-      drawCount: 36,
-    };
   }
 
   createDeferredPipeline(): GPURenderPipeline {
@@ -257,69 +278,16 @@ export class DeferredRenderer {
 
     // Things that will come from the material
     // (This is for opaque surfaces only!)
-    const cullMode: GPUCullMode = 'none';
+    const cullMode: GPUCullMode = 'back';
 
     const module = this.device.createShaderModule({
       label: 'deferred shader module',
-      code: wgsl`
-        struct Camera {
-          projection : mat4x4f,
-          view : mat4x4f,
-          position : vec3f,
-          time : f32,
-        };
-        @group(0) @binding(0) var<uniform> camera : Camera;
-
-        struct VertexInput {
-          @location(0) position : vec4f,
-          @location(1) color : vec3f,
-          @location(2) normal : vec3f,
-          @location(3) texcoord : vec2f,
-        };
-
-        struct VertexOutput {
-          @builtin(position) position : vec4f,
-          @location(1) color : vec3f,
-          @location(2) normal : vec3f,
-          @location(3) texcoord : vec2f,
-        };
-
-        @vertex
-        fn vertexMain(input : VertexInput) -> VertexOutput {
-          var output : VertexOutput;
-
-          output.position = camera.projection * camera.view * input.position;
-          output.color = input.color;
-          //output.normal = (camera.view * vec4f(input.normal, 0)).xyz;
-          output.normal = input.normal; // World space normal
-          output.texcoord = input.texcoord;
-
-          return output;
-        }
-
-        struct FragmentOutput {
-          @location(0) albedo : vec4f,
-          @location(1) normal : vec4f,
-          @location(2) metalRough : vec2f,
-          @location(3) light : vec4f,
-        };
-
-        @fragment
-        fn fragmentMain(input : VertexOutput) -> FragmentOutput {
-          var out: FragmentOutput;
-
-          out.albedo = vec4f(input.color, 1);
-          out.normal = vec4f(normalize(input.normal) * 0.5 + 0.5, 1);
-          out.metalRough = input.texcoord;
-
-          return out;
-        }
-      `
+      code: gBufferShader
     });
 
     const pipeline = this.device.createRenderPipeline({
       label: 'deferred render pipeline',
-      layout: this.device.createPipelineLayout({ bindGroupLayouts: [this.cameraBindGroupLayout] }),
+      layout: this.device.createPipelineLayout({ bindGroupLayouts: [this.frameBindGroupLayout] }),
       vertex: {
         module,
         entryPoint: 'vertexMain',
@@ -355,48 +323,32 @@ export class DeferredRenderer {
   createLightingPipeline(): GPURenderPipeline {
     const module = this.device.createShaderModule({
       label: 'lighting shader module',
-      code: wgsl`
-        const pos : array<vec2f, 3> = array<vec2f, 3>(
-          vec2f(-1, -1), vec2f(-1, 3), vec2f(3, -1));
-
-        struct VertexOutput {
-          @builtin(position) position : vec4<f32>,
-          @location(0) texcoord : vec2<f32>,
-        };
-
-        @vertex
-        fn vertexMain(@builtin(vertex_index) i: u32) -> VertexOutput {
-          let p = pos[i];
-          var output : VertexOutput;
-          output.position = vec4f(p, 0, 1);
-          output.texcoord = (vec2f(p.x, -p.y) + 1) * 0.5;
-          return output;
-        }
-
-        @fragment
-        fn fragmentMain(input : VertexOutput) -> @location(0) vec4f {
-          return vec4f(input.texcoord, 0, 1);
-        }
-      `
+      code: lightingShader
     });
 
     const pipeline = this.device.createRenderPipeline({
       label: 'lighting render pipeline',
-      layout: 'auto',
+      layout: this.device.createPipelineLayout({ bindGroupLayouts: [this.frameBindGroupLayout, this.gBufferBindGroupLayout] }),
       vertex: {
         module,
         entryPoint: 'vertexMain',
-      },
-      depthStencil: {
-        format: 'depth16unorm',
-        depthWriteEnabled: false,
-        depthCompare: 'less',
       },
       fragment: {
         module,
         entryPoint: 'fragmentMain',
         targets: [{
           format: 'rgb10a2unorm',
+          blend: {
+            color: {
+              // Additive rendering
+              srcFactor: 'one',
+              dstFactor: 'one',
+            },
+            alpha: {
+              srcFactor: 'one',
+              dstFactor: 'one',
+            }
+          }
         }],
       },
     });
@@ -406,16 +358,41 @@ export class DeferredRenderer {
 
   updateCamera(camera: Camera) {
     // AUGH! BAD!
+    const projection = mat4.create();
+    mat4.perspectiveZO(projection, Math.PI * 0.5, 1, 0.1, 20);
+
+    const invViewProjection = mat4.create();
+    mat4.multiply(invViewProjection, projection, camera.viewMatrix);
+    mat4.invert(invViewProjection, invViewProjection);
+
     const cameraArray = new Float32Array(64);
-    mat4.perspectiveZO(cameraArray, Math.PI * 0.5, 1, 0.1, 10);
+    cameraArray.set(projection, 0);
     cameraArray.set(camera.viewMatrix, 16);
-    cameraArray.set(camera.position, 32);
+    cameraArray.set(invViewProjection, 32);
+    cameraArray.set(camera.position, 48);
 
     this.device.queue.writeBuffer(this.cameraBuffer, 0, cameraArray);
   }
 
+  updateLights(t: number) {
+    // Need to update any part of the lights?
+    for (let i = 0; i < LIGHT_COUNT; ++i) {
+      const lightOffset = 16 + (i * LIGHT_STRUCT_SIZE)
+      const posRange = new Float32Array(this.lightArrayBuffer, lightOffset, 4);
+      //const colorIntensity = new Float32Array(this.lightArrayBuffer, lightOffset + 16, 4);
+
+      const r = (i / LIGHT_COUNT) * Math.PI * 2 + (t/1000);
+      posRange[0] = Math.sin(r) * 2.5;
+      posRange[1] = Math.sin(t / 1000 + (i / LIGHT_COUNT)) * 1.5;
+      posRange[2] = Math.cos(r) * 2.5;
+    }
+
+    this.device.queue.writeBuffer(this.lightBuffer, 0, this.lightArrayBuffer);
+  }
+
   render(output: GPUTexture, camera: Camera /*, content: any*/) {
     this.updateCamera(camera);
+    this.updateLights(performance.now());
 
     const encoder = this.device.createCommandEncoder();
 
@@ -426,7 +403,7 @@ export class DeferredRenderer {
     });
 
     // Draw stuff
-    gBufferPass.setBindGroup(0, this.cameraBindGroup);
+    gBufferPass.setBindGroup(0, this.frameBindGroup);
 
     gBufferPass.setPipeline(this.tempPipeline);
 
@@ -445,24 +422,35 @@ export class DeferredRenderer {
     gBufferPass.end();
 
     const lightingPass = encoder.beginRenderPass({
-      label: 'lighting pass',
-      colorAttachments: [{
-        view: this.colorAttachments[3].view,
-        loadOp: 'load',
-        storeOp: 'store',
-      }],
-      depthStencilAttachment: {
-        view: this.depthAttachment.view,
-        depthReadOnly: true,
-      }
+      label: 'deferred lighting pass',
+      colorAttachments: this.lightAttachments,
     });
 
-    // Light stuff
+    // Light deferred surfaces
 
     lightingPass.setPipeline(this.lightingPipeline);
+    lightingPass.setBindGroup(0, this.frameBindGroup);
+    lightingPass.setBindGroup(1, this.gBufferBindGroup);
     lightingPass.draw(3);
 
     lightingPass.end();
+
+    const forwardPass = encoder.beginRenderPass({
+      label: 'forward pass',
+      colorAttachments: this.lightAttachments,
+      depthStencilAttachment: {
+        view: this.depthAttachment.view,
+        depthLoadOp: 'load',
+        depthStoreOp: 'store',
+      }
+    });
+
+    // Draw forward stuff?
+    forwardPass.setBindGroup(0, this.frameBindGroup);
+
+    this.lightSpriteRenderer.render(forwardPass, LIGHT_COUNT);
+
+    forwardPass.end();
 
     const outputPass = encoder.beginRenderPass({
       label: 'output pass',
@@ -473,16 +461,11 @@ export class DeferredRenderer {
         storeOp: 'store',
         clearValue: [0.5, 0, 1, 1],
       }],
-      depthStencilAttachment: {
-        view: this.depthAttachment.view,
-        depthLoadOp: 'load',
-        depthStoreOp: 'store',
-      }
     });
 
-    // Combine RGB/Lighting attachments
-
-    // Draw forward stuff
+    // Tone map
+    // Blur
+    // Maybe post-process AA?
 
     // Profit???
 
---
+++ src/shaders/deferred.ts
@@ -0,0 +1,154 @@
+//import { wgsl } from 'https://cdn.jsdelivr.net/npm/wgsl-preprocessor@1.0/wgsl-preprocessor.js'
+
+export const cameraStruct = /* wgsl */`
+  struct Camera {
+    projection : mat4x4f,
+    view : mat4x4f,
+    invViewProjection: mat4x4f,
+    position : vec3f,
+    time : f32,
+  };
+`;
+
+export const lightStruct = /* wgsl */`
+  struct PointLight {
+    position : vec3<f32>,
+    range : f32,
+    color : vec3<f32>,
+    intensity: f32,
+  };
+
+  struct Lights {
+    pointLightCount: u32,
+    pointLights: array<PointLight>,
+  };
+`;
+
+export const gBufferShader = /* wgsl */`
+  ${cameraStruct}
+  @group(0) @binding(0) var<uniform> camera : Camera;
+
+  struct VertexInput {
+    @location(0) position : vec4f,
+    @location(1) color : vec3f,
+    @location(2) normal : vec3f,
+    @location(3) texcoord : vec2f,
+  };
+
+  struct VertexOutput {
+    @builtin(position) position : vec4f,
+    @location(1) color : vec3f,
+    @location(2) normal : vec3f,
+    @location(3) texcoord : vec2f,
+  };
+
+  @vertex
+  fn vertexMain(input : VertexInput) -> VertexOutput {
+    var output : VertexOutput;
+
+    output.position = camera.projection * camera.view * input.position;
+    output.color = input.color;
+    output.normal = input.normal; // World space normal
+    output.texcoord = input.texcoord;
+
+    return output;
+  }
+
+  struct FragmentOutput {
+    @location(0) albedo : vec4f,
+    @location(1) normal : vec4f,
+    @location(2) metalRough : vec2f,
+    @location(3) light : vec4f,
+  };
+
+  const lightAmbient = vec3f(0.1);
+
+  @fragment
+  fn fragmentMain(input : VertexOutput) -> FragmentOutput {
+    var out: FragmentOutput;
+
+    out.albedo = vec4f(input.color, 1);
+    out.normal = vec4f(normalize(input.normal) * 0.5 + 0.5, 1);
+    out.metalRough = input.texcoord;
+
+    // Add emissive here too eventually
+    out.light = vec4f(input.color * lightAmbient, 1);
+
+    return out;
+  }
+`;
+
+export const lightingShader = /* wgsl */`
+  ${cameraStruct}
+  @group(0) @binding(0) var<uniform> camera : Camera;
+
+  ${lightStruct}
+  @group(0) @binding(1) var<storage> lights : Lights;
+
+  const pos : array<vec2f, 3> = array<vec2f, 3>(
+    vec2f(-1, -1), vec2f(-1, 3), vec2f(3, -1));
+
+  @vertex
+  fn vertexMain(@builtin(vertex_index) i: u32) -> @builtin(position) vec4f {
+    return vec4f(pos[i], 0, 1);
+  }
+
+  @group(1) @binding(0) var colorTexture: texture_2d<f32>;
+  @group(1) @binding(1) var normalTexture: texture_2d<f32>;
+  @group(1) @binding(2) var metalRoughTexture: texture_2d<f32>;
+  @group(1) @binding(3) var depthTexture: texture_depth_2d;
+
+  fn worldPosFromDepth(texcoord: vec2f, depth: f32) -> vec3f {
+    let clipSpacePos = vec4f((texcoord * 2 - 1) * vec2f(1, -1), depth, 1);
+    let worldPos = camera.invViewProjection * clipSpacePos;
+    return (worldPos.xyz / worldPos.w);
+  }
+
+  const lightDir = vec3f(0.25, 0.5, 1.0);
+  const dirColor = vec3f(1);
+
+  @fragment
+  fn fragmentMain(@builtin(position) pos : vec4f) -> @location(0) vec4f {
+    // TODO: Generate world position from coord
+
+    let targetSize = vec2f(textureDimensions(colorTexture, 0));
+    let pixelCoord = vec2u(pos.xy);
+
+    let color = textureLoad(colorTexture, pixelCoord, 0);
+    let normal = textureLoad(normalTexture, pixelCoord, 0);
+    let metalRough = textureLoad(metalRoughTexture, pixelCoord, 0);
+    let depth = textureLoad(depthTexture, pixelCoord, 0);
+    let worldPos = worldPosFromDepth((pos.xy / targetSize), depth);
+
+    var Lo = vec3f(0);
+
+    // Simple directional light
+    /*{
+      let N = normalize(2 * normal.xyz - 1);
+      let L = normalize(lightDir);
+      let NDotL = max(dot(N, L), 0.0);
+      Lo += color.rgb * NDotL * dirColor;
+    }*/
+
+    // Point lights
+    for (var i: u32 = 0; i < lights.pointLightCount; i++) {
+      let light = &lights.pointLights[i];
+      let range = (*light).range;
+      let lightColor = (*light).color;
+      let lightIntensity = (*light).color;
+
+      let worldToLight = (*light).position - worldPos;
+
+      let N = normalize(2 * normal.xyz - 1);
+      let L = normalize(worldToLight);
+      let NDotL = max(dot(N, L), 0.0);
+
+      let dist = length(worldToLight);
+      let atten = clamp(1.0 - pow(dist / range, 4.0), 0.0, 1.0) / pow(dist, 2.0);
+
+      Lo += color.rgb * NDotL * lightColor * lightIntensity * atten;
+    }
+
+    return vec4f(Lo, 1);
+  }
+`;
